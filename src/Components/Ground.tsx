9183c1193e0d54a8e6445aa9595c02c935c396d6
EvtheDev <evthedev22@gmail.com>
2022-03-08 12:22:12 -0600

    wip svg grass

---
+++ src/Components/Ground.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as Grass } from '../Assets/morphing-diamonds.svg';
+
+export default function Ground(){
+    return(
+        <Grass className="ground" />
+    );
+}
--- src/Components/Stage.tsx
+++ src/Components/Stage.tsx
@@ -1,32 +1,13 @@
 import React, { useEffect, useState, KeyboardEvent } from 'react';
 import { GiFlyingFlag } from 'react-icons/gi'
+import Ground from './Ground';
 
 export default function Stage(){
-    const [scrollLeft, setScrollLeft] = useState(false);
-
-    useEffect(() => {
-        if(scrollLeft){
-            let stage = document.querySelector<HTMLElement>('.stage');
-            stage!.style.transform = "translateX(20px)"; // this is replacing the style completely, need to append
-        }
-    }, [scrollLeft]);
-
-    function handleKeyDown(event: KeyboardEvent){
-        console.log('keypressed');
-        if(event.key === 'ArrowLeft'){
-            setScrollLeft(true);
-        }
-    }
-
-    function handleKeyUp(event: KeyboardEvent){
-        if(event.key === 'ArrowLeft'){
-            setScrollLeft(false);
-        }
-    }
 
     return(
         <div className="stage" tabIndex={0}>
             <GiFlyingFlag className="middle-flag" />
+            <Ground />
         </div>
     );
 }

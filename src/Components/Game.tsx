0bf6ee9923ad5abd89a155e148d1c6a2b75492c2
Evan Marshall <73307083+EvanMarshallj@users.noreply.github.com>
2022-03-08 20:11:23 -0600

    Set the stage (#1)
    * Basic pointer scrolling and background
    
    * WIP using arrows
    
    added colours, framed window on screen
    
    * WIP
    
    * wip svg grass
    
    * Set towers and ground
    
    Co-authored-by: EvtheDev <evthedev22@gmail.com>

---
+++ src/Components/Game.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Window from './Window';
+
+export default function Game(){
+    
+    return(
+        <div className="background">
+            <Window />  
+        </div>
+    );
+}

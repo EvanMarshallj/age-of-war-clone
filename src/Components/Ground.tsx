25ebe01529768c69b5706bf1132bd6160c3be846
EvtheDev <evthedev22@gmail.com>
2022-03-08 20:07:59 -0600

    Set towers and ground

--- src/Components/Ground.tsx
+++ src/Components/Ground.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ReactComponent as Grass } from '../Assets/morphing-diamonds.svg';
 
 export default function Ground(){
     return(
-        <Grass className="ground" />
+        <>
+            <div className="ground"></div>
+        </>
     );
 }
--- src/Components/Stage.tsx
+++ src/Components/Stage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, KeyboardEvent } from 'react';
-import { GiFlyingFlag } from 'react-icons/gi'
+import { GiFlyingFlag, GiStoneTower, GiWhiteTower } from 'react-icons/gi'
 import Ground from './Ground';
 
 export default function Stage(){
@@ -7,6 +7,8 @@ export default function Stage(){
     return(
         <div className="stage" tabIndex={0}>
             <GiFlyingFlag className="middle-flag" />
+            <GiStoneTower className='friendly-tower tower'/>
+            <GiWhiteTower className='enemy-tower tower' />
             <Ground />
         </div>
     );
